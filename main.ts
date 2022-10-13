e15da0000f12f52073e522339c7fe49a3a2a4c43
Eminsha <113388072+Eminsha@users.noreply.github.com>
2022-10-13 14:25:34 -0700

    made boat array

--- main.ts
+++ main.ts
@@ -12,33 +12,16 @@ function moveBoat (boatArray: Sprite[]) {
         Cursor.setFlag(SpriteFlag.Invisible, false)
         moveBoatFlag = 0
     }
+    if (controller.B.isPressed()) {
+    	
+    }
 }
 let iterator = 0
 let Cursor: Sprite = null
 let moveBoatFlag = 0
-moveBoatFlag = 1
-tiles.setCurrentTilemap(tilemap`level1`)
-Cursor = sprites.create(img`
-    d d d d d . . . . . . d d d d d 
-    d d d . . . . . . . . . . d d d 
-    d d . . . . . . . . . . . . d d 
-    d . . . . . . . . . . . . . . d 
-    d . . . . . . . . . . . . . . d 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    d . . . . . . . . . . . . . . d 
-    d . . . . . . . . . . . . . . d 
-    d d . . . . . . . . . . . . d d 
-    d d d . . . . . . . . . . d d d 
-    d d d d d . . . . . . d d d d d 
-    `, SpriteKind.Player)
-grid.snap(Cursor)
-grid.moveWithButtons(Cursor)
-let smallBoatArray = [sprites.create(img`
+let boatRotateArray = ["up", "up", "up"]
+let CurrentBoat = 0
+let boatSpriteArray = [[sprites.create(img`
     . . . . . b b b b b b . . . . . 
     . . . b b 9 9 9 9 9 9 b b . . . 
     . . b b 9 9 9 9 9 9 9 9 b b . . 
@@ -72,9 +55,82 @@ let smallBoatArray = [sprites.create(img`
     . . b d 5 d 3 3 3 3 5 5 b b . . 
     . . . b b 5 5 5 5 5 5 b b . . . 
     . . . . . b b b b b b . . . . . 
-    `, SpriteKind.Player)]
+    `, SpriteKind.Player)], [sprites.create(img`
+    . . . . . b b b b b b . . . . . 
+    . . . b b 9 9 9 9 9 9 b b . . . 
+    . . b b 9 9 9 9 9 9 9 9 b b . . 
+    . b b 9 d 9 9 9 9 9 9 9 9 b b . 
+    . b 9 d 9 9 9 9 9 1 1 1 9 9 b . 
+    b 9 d d 9 9 9 9 9 1 1 1 9 9 9 b 
+    b 9 d 9 9 9 9 9 9 1 1 1 9 9 9 b 
+    b 9 3 9 9 9 9 9 9 9 9 9 1 9 9 b 
+    b 5 3 d 9 9 9 9 9 9 9 9 9 9 9 b 
+    b 5 3 3 9 9 9 9 9 9 9 9 9 d 9 b 
+    b 5 d 3 3 9 9 9 9 9 9 9 d d 9 b 
+    . b 5 3 3 3 d 9 9 9 9 d d 5 b . 
+    . b d 5 3 3 3 3 3 3 3 d 5 b b . 
+    . . b d 5 d 3 3 3 3 5 5 b b . . 
+    . . . b b 5 5 5 5 5 5 b b . . . 
+    . . . . . b b b b b b . . . . . 
+    `, SpriteKind.Player), sprites.create(img`
+    . . . . . b b b b b b . . . . . 
+    . . . b b 9 9 9 9 9 9 b b . . . 
+    . . b b 9 9 9 9 9 9 9 9 b b . . 
+    . b b 9 d 9 9 9 9 9 9 9 9 b b . 
+    . b 9 d 9 9 9 9 9 1 1 1 9 9 b . 
+    b 9 d d 9 9 9 9 9 1 1 1 9 9 9 b 
+    b 9 d 9 9 9 9 9 9 1 1 1 9 9 9 b 
+    b 9 3 9 9 9 9 9 9 9 9 9 1 9 9 b 
+    b 5 3 d 9 9 9 9 9 9 9 9 9 9 9 b 
+    b 5 3 3 9 9 9 9 9 9 9 9 9 d 9 b 
+    b 5 d 3 3 9 9 9 9 9 9 9 d d 9 b 
+    . b 5 3 3 3 d 9 9 9 9 d d 5 b . 
+    . b d 5 3 3 3 3 3 3 3 d 5 b b . 
+    . . b d 5 d 3 3 3 3 5 5 b b . . 
+    . . . b b 5 5 5 5 5 5 b b . . . 
+    . . . . . b b b b b b . . . . . 
+    `, SpriteKind.Player), sprites.create(img`
+    . . . . . b b b b b b . . . . . 
+    . . . b b 9 9 9 9 9 9 b b . . . 
+    . . b b 9 9 9 9 9 9 9 9 b b . . 
+    . b b 9 d 9 9 9 9 9 9 9 9 b b . 
+    . b 9 d 9 9 9 9 9 1 1 1 9 9 b . 
+    b 9 d d 9 9 9 9 9 1 1 1 9 9 9 b 
+    b 9 d 9 9 9 9 9 9 1 1 1 9 9 9 b 
+    b 9 3 9 9 9 9 9 9 9 9 9 1 9 9 b 
+    b 5 3 d 9 9 9 9 9 9 9 9 9 9 9 b 
+    b 5 3 3 9 9 9 9 9 9 9 9 9 d 9 b 
+    b 5 d 3 3 9 9 9 9 9 9 9 d d 9 b 
+    . b 5 3 3 3 d 9 9 9 9 d d 5 b . 
+    . b d 5 3 3 3 3 3 3 3 d 5 b b . 
+    . . b d 5 d 3 3 3 3 5 5 b b . . 
+    . . . b b 5 5 5 5 5 5 b b . . . 
+    . . . . . b b b b b b . . . . . 
+    `, SpriteKind.Player)]]
+tiles.setCurrentTilemap(tilemap`level1`)
+moveBoatFlag = 1
+Cursor = sprites.create(img`
+    d d d d d . . . . . . d d d d d 
+    d d d . . . . . . . . . . d d d 
+    d d . . . . . . . . . . . . d d 
+    d . . . . . . . . . . . . . . d 
+    d . . . . . . . . . . . . . . d 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    d . . . . . . . . . . . . . . d 
+    d . . . . . . . . . . . . . . d 
+    d d . . . . . . . . . . . . d d 
+    d d d . . . . . . . . . . d d d 
+    d d d d d . . . . . . d d d d d 
+    `, SpriteKind.Player)
+grid.snap(Cursor)
+grid.moveWithButtons(Cursor)
 game.onUpdate(function () {
     if (moveBoatFlag == 1) {
-        moveBoat(smallBoatArray)
+        moveBoat(boatSpriteArray[CurrentBoat])
     }
 })
